46a06873cb1b5c499a5ea01393e41e58f6f8b714
Duplic8e <abood.rules@gmail.com>
2025-06-01 17:52:35 -0230

    Revert "integrate MD changes"
    This reverts commit 520166470ed3680f592cbda143c49fee818328a2.

--- lib/features/user_profile/domain/repositories/user_profile_repository.dart
+++
@@ -1,9 +0,0 @@
-abstract class UserProfileRepository {
-  Future<void> createUserProfile({
-    required String userId,
-    required String email,
-    required Set<String> preferences,
-  });
-// Future<UserProfile?> getUserProfile(String userId);
-// Future<void> updateUserProfile(String userId, Map<String, dynamic> data);
-}
